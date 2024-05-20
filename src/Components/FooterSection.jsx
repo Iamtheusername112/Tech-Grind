3b7ecad98d86f71fff8042921cd0e584749742ad
Francis Iwu <apkfem@gmail.com>
2024-05-20 13:11:42 +0200

    implement tailwindcss

---
+++ src/Components/FooterSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterSection = () => {
+  return <div>FooterSection</div>;
+};
+
+export default FooterSection;
---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeroSection = () => {
+  return <div>HeroSection goes here</div>;
+};
+
+export default HeroSection;

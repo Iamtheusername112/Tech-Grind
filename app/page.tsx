e9b2118f78bee16378121a1cb1d71704ef206eaf
Francis Iwu <apkfem@gmail.com>
2024-06-13 21:57:14 +0200

    implement dark and light mode using schadcn ui library

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { DarkmodeHandler } from "./components/DarkmodeHandler";
+
+export default function Home() {
+  return (
+    <div className="flex justify-between p-10 items-center">
+      <Button>Get Started</Button>
+      <DarkmodeHandler />
+    </div>
+  );
+}

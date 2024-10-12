9e3349534826c5cdad331a4088459ff2b2ab4e66
Francis Iwu <apkfem@gmail.com>
2024-10-12 11:30:46 +0200

    Developing the contact us component

--- app/check-us-out/contact/page.tsx
+++ app/check-us-out/contact/page.tsx
@@ -3,14 +3,61 @@ import React from "react";
 
 function ContactUs() {
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-semibold mb-4">Contact Us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem
-        dicta illo facere quo rerum voluptatum nesciunt fugit! Mollitia quae
-        aliquid reprehenderit voluptas incidunt quod dolorum eaque perferendis
-        eveniet soluta.
-      </p>
+    <div className="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-indigo-700 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="text-white relative px-4 py-10 bg-indigo-400 shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="text-center pb-6">
+            <h1 className="text-3xl">Contact Us!</h1>
+
+            <p className="text-gray-300">
+              Fill up the form below to send us a message.
+            </p>
+          </div>
+
+          <form>
+            <input
+              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Name"
+              name="name"
+            />
+
+            <input
+              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="email"
+              placeholder="Email"
+              name="email"
+            />
+
+            <input
+              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Subject"
+              name="_subject"
+            />
+
+            <textarea
+              className="shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              typeof="text"
+              placeholder="Type your message here..."
+              name="message"
+            />
+
+            <div className="flex justify-between">
+              <input
+                className="shadow bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+                value="Send ➤"
+              />
+              <input
+                className="shadow bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="reset"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- app/check-us-out/events/page.tsx
+++ app/check-us-out/events/page.tsx
@@ -2,10 +2,42 @@ import React from "react";
 
 function Events() {
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-semibold mb-4">
-        Check out our up mcomming events{" "}
-      </h1>
+    <div className="">
+      {/*
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+*/}
+
+      <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+
+        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+              Let us find your
+              <strong className="block font-extrabold text-rose-500">
+                {" "}
+                Forever Home.{" "}
+              </strong>
+            </h1>
+
+            <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+              illo tenetur fuga ducimus numquam ea!
+            </p>
+
+            <div className="mt-8 flex flex-wrap gap-4 text-center">
+              <a
+                href="#"
+                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+              >
+                Register for event
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
